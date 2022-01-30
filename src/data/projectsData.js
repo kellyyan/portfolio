f48b2a0ad8144500fb158ca322397017a6da75c2
Kelly Yan <yan.kelly11@gmail.com>
2022-01-29 21:46:47 -0800

    changed order of projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,21 +2,21 @@ const projectData = [
     {
         "id" : "1",
         "field" : "UI/UX",
-        "type" : "Project",
-        "title" : "Coders SB",
-        "description" : "Coders SB needed a website redesign.",
-        "image" : "coders_sb_thumbnail.svg",
-        "url" : "/coders-sb"
-    },
-    {
-        "id" : "2",
-        "field" : "UI/UX",
         "type" : "Internship",
         "title" : "F*ck It Denim",
         "description" : "A website for a company striving to make size inclusive jeans.",
         "image" : "fid_thumbnail.svg",
         "url" : "/fid"
     },
+    {
+        "id" : "2",
+        "field" : "UI/UX",
+        "type" : "Project",
+        "title" : "Coders SB",
+        "description" : "Coders SB needed a website redesign.",
+        "image" : "coders_sb_thumbnail.svg",
+        "url" : "/coders-sb"
+    }
 ]
 
 export default projectData
